04b9a77e26886dbba8bfc4907a3502ef27f63062
AlbanoRavenPOGI <onlyven711@gmail.com>
2026-07-18 11:13:43 +0800

    Commit test

--- ConsoleApp1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -10,15 +10,21 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    public IActionResult AboutMe()
+    {
+        return View();
+    }
+    public IActionResult Skills()
+    {
+        return View();
+    }
+    public IActionResult Projects()
+    {
+        return View();
+    }
+    public IActionResult Contacts()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
