b4825c7389010086359901f0339e0e14db396169
AlbanoRavenPOGI <onlyven711@gmail.com>
2026-07-18 11:58:09 +0800

    Commit

--- ConsoleApp1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -12,18 +12,35 @@ public class HomeController : Controller
     }
     public IActionResult AboutMe()
     {
+        ViewData["About"]= " I am currently studying Bachelor of Science in Information Technology.\r\n                I enjoy creating desktop and web applications while continuously improving\r\n                my programming skills through school projects and self-learning.";
+        ViewData["AboutMe"] = " My goal is to become a full-stack software developer and build systems that\r\n                make people's work easier. While I'm in my free time, I enjoy programming,\r\n                reading, gaming, and learning new technologies.";
+        ViewData["Education"] = "BS Information Technolog";
+        ViewData["CareerGoal"] = "Full Stack Developer";
+        ViewData["Interest"] = "Programming, Reading, Gaming";
         return View();
     }
     public IActionResult Skills()
     {
+        ViewData["ProgrammingLanguages"] = "C#, Java, Python, VB.NET";
+        ViewData["Web"] = " HTML, CSS, Bootstrap, ASP.NET MVC";
+        ViewData["Databases"] = "   MySQL, MS Access";
+        ViewData["Tools"] = "Visual Studio, VS Code, Pycharm, GitHub";
         return View();
     }
     public IActionResult Projects()
     {
+        ViewData["SalesInventory"] = "Desktop application that manages products, inventory, and sales transactions.";
+        ViewData["ParkingManagement"] = "Console-based parking system with receipt generation, and payment computation.";
+        ViewData["LoanApplication"] = "A platform that allows users to apply for loans and check their approval status. It computes Interest, Monthly Amortization, and Total Amount Payable.";
         return View();
     }
     public IActionResult Contacts()
     {
+        ViewData["Email"] = "rondoloravenjoe@gmail.com";
+        ViewData["Mobile"] = "0976-228-6079";
+        ViewData["Github"] = "github.com/AlbanoRavenPOGI";
+        ViewData["LinkedIn"] = "linkedin.com/in/raven";
+        ViewData["Facebook"] = "Raven Alban?o";
         return View();
     }
 
